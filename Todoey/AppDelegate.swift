ae241e2b27390d5eca7fd991410326af408b8f2d
Amad Salmon <amad.salmon@icloud.com>
2020-07-30 21:57:11 +0200

    initial commit

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  Destini
+//  Todoey
 //
-//  Created by Philipp Muellauer on 01/09/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Amad Salmon on 30/07/2020.
+//  Copyright © 2020 Amad Salmon. All rights reserved.
 //
 
 import UIKit
